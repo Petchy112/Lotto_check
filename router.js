fd296002828d858114fbb92c5083967dde613c14
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-05-12 14:50:57 +0700

    add prize

--- router.js
+++ router.js
@@ -1,13 +1,19 @@
+const { response } = require('express')
 const express = require('express')
 const router = express.Router()
 var lotto = require('./Lotto')
 
 router.get('/',(req,res) => {
-    lotto.find().exec((err,result) => {
-        res.json({Message:'Done'})
-        console.log('Done')
+    lotto.find().exec((err,data) => {
+        res.send(data)
     })
 })
-
+router.post('/add', async (req,res) => {
+    var obj = new lotto (req.body);
+    await obj.save((err,data) => {
+        if (err) return res.status(400) , err;
+        res.json({Message:'Inserted'})
+    })
+})
 
 module.exports = router
