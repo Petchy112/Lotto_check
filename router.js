af0d198ae4ca6874fc51570a956f77b2b7f335f8
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-05-13 20:00:35 +0700

    find

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 var lotto = require('./Lotto')
+var news = require('./Lotto')
 
 router.get('/',(req,res) => {
     lotto.find().exec((err,data) => {
@@ -14,5 +15,17 @@ router.post('/add', async (req,res) => {
         res.json({Message:'Inserted'})
     })
 })
-
+router.get('/find/:?',_id, async (req,res) => {
+     await lotto.findById(req.params._id).exec((err,data) => {
+        if (err) return res.json('Not found')
+        res.send(result)
+     })
+})   
+router.post('/news',async(req,res) => {
+    var newHead = new news (req.body);
+    await obj.save((err,data) => {
+        if (err) return res.status(404) , err;
+        res.json({Message:'Inserted News'})
+    })
+})
 module.exports = router
