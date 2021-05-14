3fa9cf56f992cbb921d8982f35a84a4bac18ef11
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-05-14 10:41:06 +0700

    check lotto last 2 3 , head 3

--- Lotto.js
+++ Lotto.js
@@ -1,40 +1,40 @@
 var mongoose = require('mongoose');
-var lottoSchema = mongoose.Schema(
-    first =
-        num = {type : String}
-    ,
-    tailTwo =
-        num = {type : String}
-    ,
-    second = [
-        num = {type : String, }
-    ],
-    third = [
-        num = {type : String}
-    ],
-    forth = [
-        num = {type : String}
-    ],
-    fifth = [
-        num = {type : String}
-    ],
-    headThree = [
-        num = {type : String}
-    ],
-    tailThree = [
-        num = {type : String}
-    ],
-    
-)
-
-var newsSchema = mongoose.Schema({
-    News : [
-        header = { type :String }
-        
-    ]
+var lottoSchema = mongoose.Schema({
+    first : {
+        num : {type : String} ,
+        myText : {type :String}
+    },
+    nearFirst: {
+        num : {type:String},
+    },
+    tailTwo : {
+        num : {type : String}
+    },
+    second : {
+        num : {type : String, },
+        myText : {type :String}
+    },
+    third : {
+        num : {type : String},
+        myText : {type :String}
+    },
+    forth : {
+        num : {type : String},
+        myText : {type :String}
+    },
+    fifth : {
+        num : {type : String},
+    },
+    headThree : {
+        num : {type : String},
+        myText : {type :String}
+    },
+    tailThree : {
+        num : {type : String},
+        myText : {type :String}
+    },
 })
 var Lotto = mongoose.model('Prize',lottoSchema)
-var news = mongoose.model('News',newsSchema)
 
-module.exports = news
+
 module.exports = Lotto
--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 var lotto = require('./Lotto')
 var news = require('./Lotto')
-var insertNum;
+
     
 router.get('/',(req,res) => {
     lotto.find().exec((err,data) => {
@@ -17,9 +17,20 @@ router.post('/add', async (req,res) => {
     })
 })
 router.get('/find', async (req,res) => {
-     lotto.find((req.body),(err,result) => {
+    const tailTwo  = req.query.num.slice(-2);
+    const headThree = req.query.num.slice(0,3);
+    const tailThree = req.query.num.slice(3,6);
+    const first = req.query.num;
+    const second = req.query.num;
+    const third = req.query.num;
+    const forth = req.query.num;
+    const fifth = req.query.num;
+    console.log(first);
+    console.log(tailTwo);
+    await lotto.find(( { $or: [ {headThree},{tailTwo},{tailThree},{first},{second},{third},{forth},{fifth} ] } ),(err,result) => {
+
         if (err) return res.json('not found').status(404);
-        res.send(result);
+        res.json(result);
      });
 })
 
